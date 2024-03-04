52ad025057fe637a6a312c850fdaa6dffd494b65
Frederik Gailly <Frederik.Gailly@ugent.be>
2024-03-04 13:04:05 +0100

    upload code lecture 1 and 2

---
+++ les1/Eerste_Programma.py
@@ -0,0 +1,9 @@
+term1_str = input("Getal1 ?")
+term2_str = input("Getal2 ?")
+
+term1 = int(term1_str)
+term2 = int(term2_str)
+
+sum = term1 / term2
+
+print(sum)
---
+++ les2/area_circle.py
@@ -0,0 +1,12 @@
+# radius of circle
+radius = 2.73
+
+# value of π
+pi = 3.141592653589793
+
+area = pi * radius**2
+
+print("The area of a circle with radius " + str(radius)
+      + " is " + str(area) + ".")
+
+print("The area of a circle with radius", radius, "is", area,".")
